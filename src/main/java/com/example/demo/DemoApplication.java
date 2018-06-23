c62c5d767d3fa575879d108dcd6240e1773059cb
endon058 <877058128@qq.com>
2018-06-23 09:59:53 +0800

    添加了 部分注释

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// Eureka 服务注册中心
 @EnableEurekaServer
 @SpringBootApplication
 public class DemoApplication {
